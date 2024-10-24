d59ab44901355360e6017c6218983d2826c4bbcf
mbp4 <mblanpon@myuax.com>
2024-10-24 09:44:27 +0200

    Initial commit

---
+++ src/test/java/org/example/jurassic/JurassicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jurassic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurassicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
