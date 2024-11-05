218dd907016d7b325c9c9e38dd104b7511447b3d
mbp4 <mblanpon@myuax.com>
2024-11-05 19:30:43 +0100

    aop y base de datos

--- src/main/java/org/example/jurassic/model/entidades/Dinosaurio.java
+++ src/main/java/org/example/jurassic/model/entidades/Dinosaurio.java
@@ -1,4 +1,16 @@
 package org.example.jurassic.model.entidades;
 
-public class Dinosaurio {
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+public class Dinosaurio implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
 }
