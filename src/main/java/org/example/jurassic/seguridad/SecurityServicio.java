e9eae3c0f280567bef670274ce533409de92c2ce
mbp4 <mblanpon@myuax.com>
2024-11-05 19:26:46 +0100

    aop

---
+++ src/main/java/org/example/jurassic/seguridad/SecurityServicio.java
@@ -0,0 +1,10 @@
+package org.example.jurassic.seguridad;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class SecurityServicio {
+
+}
