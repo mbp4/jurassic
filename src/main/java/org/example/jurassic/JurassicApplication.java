a2f80716fed2eeec2711acc45ed604f0e4809f1a
mbp4 <mblanpon@myuax.com>
2024-11-05 19:15:17 +0100

    main para abrir el html nada mas empezar

--- src/main/java/org/example/jurassic/JurassicApplication.java
+++ src/main/java/org/example/jurassic/JurassicApplication.java
@@ -2,12 +2,37 @@ package org.example.jurassic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class JurassicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JurassicApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void openBrowser() {
+        String os = System.getProperty("os.name").toLowerCase();
+        try {
+            if (os.contains("win")) {
+                // Windows
+                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://localhost:8080");
+            } else if (os.contains("mac")) {
+                // Mac
+                Runtime.getRuntime().exec("open http://localhost:8080");
+            } else if (os.contains("nix") || os.contains("nux")) {
+                // Linux
+                Runtime.getRuntime().exec("xdg-open http://localhost:8080");
+            } else {
+                System.err.println("Sistema operativo no soportado para abrir el navegador.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
