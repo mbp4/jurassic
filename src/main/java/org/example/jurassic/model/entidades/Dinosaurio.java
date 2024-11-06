9779d5f47653db977bb89f5f70fc6e1968fe322c
mbp4 <mblanpon@myuax.com>
2024-11-06 08:42:48 +0100

    base de datos creada

--- src/main/java/org/example/jurassic/model/entidades/Dinosaurio.java
+++ src/main/java/org/example/jurassic/model/entidades/Dinosaurio.java
@@ -9,8 +9,21 @@ import java.io.Serializable;
 
 public class Dinosaurio implements Serializable {
 
+    //id autogenerado
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    //alimentacion del dinosaurio: herviboro, carniboro, etc
+    @Column(nullable = false)
+    private String alimentacion;
+
+    //nombre del dinosaurio
+    @Column(nullable = false)
+    private String nombre;
+
+    //tipo o familia a la que pertenece el dinosaurio: T-Rex, Diplodocus
+    @Column(nullable = false)
+    private String tipo;
 
 }
