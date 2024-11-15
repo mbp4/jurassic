d21b3269d7f57d5033a513a023897c4d607e0bdb
Sira <sgonzmad@myuax.com>
2024-11-15 18:06:55 +0100

    quitar imports no usados

--- src/main/java/org/example/jurassic/seguridad/SecurityServicio.java
+++ src/main/java/org/example/jurassic/seguridad/SecurityServicio.java
@@ -1,16 +1,7 @@
 package org.example.jurassic.seguridad;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDateTime;
 
 @Aspect
 @Configuration
